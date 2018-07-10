fa71620c9ca678f483002790e2c3ac70b2358605
Alexandr Plodnik <plodnik@icloud.com>
2018-07-10 13:08:03 +0300

    Order Summary

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import classes from './Modal.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => {
     const modalStatus = props.show ? classes.Shown : classes.Hidden;
 
     return (
-        <div className={`${classes.Modal} ${modalStatus}`}>
-            {props.children}
-        </div>
+        <Aux>
+            <Backdrop show={props.show} clicked={props.modalClosed}/>
+            <div className={[classes.Modal, modalStatus].join(' ')}>
+                {props.children}
+            </div>
+        </Aux>
     );
 };
 
