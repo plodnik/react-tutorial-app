87860a32eefd5412b821cd5178b1db71f79d3778
Alexandr Plodnik <plodnik@icloud.com>
2018-07-11 19:23:51 +0300

    Add lifecycle hook in Modal Component and change folder structure

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 import classes from './OrderSummary.css';
 
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+    
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span className={classes.IngredientName}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span className={classes.IngredientName}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>
             );
         });
-
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul className={classes.OrderSummaryList}>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul className={classes.OrderSummaryList}>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }    
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Modal.css';
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => {
-    const modalStatus = props.show ? classes.Shown : classes.Hidden;
+class Modal extends Component {
+    shouldComponentUpdate (nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+    
+    render () {
+        const modalStatus = this.props.show ? classes.Shown : classes.Hidden;
+        return (
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/>
+                <div className={[classes.Modal, modalStatus].join(' ')}>
+                    {this.props.children}
+                </div>
+            </Aux>
+        )
+    }
+}
 
-    return (
-        <Aux>
-            <Backdrop show={props.show} clicked={props.modalClosed}/>
-            <div className={[classes.Modal, modalStatus].join(' ')}>
-                {props.children}
-            </div>
-        </Aux>
-    );
-};
-
-export default modal;
+export default Modal;
