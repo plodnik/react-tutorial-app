67bcb33bc258e1d2e2af906dcd7d01d6cbe0e218
Alexandr Plodnik <plodnik@icloud.com>
2018-07-22 12:03:35 +0300

    Create routing

--- src/components/Navigation/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItem/NavigationItem.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 import classes from './NavigationItem.css';
 
-const navigationItem = (props) => {
-    const linkActiveStatus = (props.active) ? classes.active : null;
-
-    return (
-            <li className={classes.NavigationItem}>
-                <a 
-                    href={props.link}
-                    className={linkActiveStatus}>{props.children}</a>
-            </li>
-    );
-};
+const navigationItem = (props) => (
+    <li className={classes.NavigationItem}>
+        <NavLink 
+            to={props.link}
+            activeClassName={classes.active}
+            exact={props.exact}>{props.children}</NavLink>
+    </li>
+);
 
 export default navigationItem;
