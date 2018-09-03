34d5198c15c4ff3dccdcf110d7f08b2ef21da8d6
Alexandr Plodnik <plodnik@icloud.com>
2018-09-03 16:40:33 +0300

    Some changes

---
+++ src/store/actions/auth.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+import * as actionTypes from './actionTypes';
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (authData) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        authData: authData
+    };
+};
+
+export const authFail = (error) => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyCOs8pxCLtJ1nKyAhhDuUD4PFYt5cLJpFE';
+        if (!isSignup)
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCOs8pxCLtJ1nKyAhhDuUD4PFYt5cLJpFE';
+        axios.post(url, authData)
+        .then(response => {
+            console.log(response);
+            dispatch(authSuccess(response.data));
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch(authFail(err));
+        });
+    };
+};
